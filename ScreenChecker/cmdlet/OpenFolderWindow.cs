375e3c61ffef940338b65f52b5302d4930173f6a
tqji31001 <tqji31001@gmail.com>
2025-05-25 16:15:35 +0900

    Update FolderWindow class.

---
+++ ScreenChecker/cmdlet/OpenFolderWindow.cs
@@ -0,0 +1,40 @@
+﻿using ScreenChecker.Lib.Folder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenChecker.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Open, "FolderWindow")]
+    public class OpenFolderWindow : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0)]
+        public string FolderPath { get; set; }
+
+        [Parameter(Position = 1)]
+        public string ItemName { get; set; }
+
+        private string _currentDirectory = null;
+
+        protected override void BeginProcessing()
+        {
+            //  Set current directory. (tempolary)
+            _currentDirectory = System.Environment.CurrentDirectory;
+            Environment.CurrentDirectory = this.SessionState.Path.CurrentFileSystemLocation.Path;
+        }
+
+        protected override void ProcessRecord()
+        {
+            FolderControl.Open(FolderPath, ItemName);
+        }
+
+        protected override void EndProcessing()
+        {
+            //  Restore current directory.
+            Environment.CurrentDirectory = _currentDirectory;
+        }
+    }
+}
